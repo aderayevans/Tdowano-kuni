8bf46afe4c0b414c7c43246b587bc58949c79cb0
aderayevans <aderayevans@gmail.com>
2023-08-08 04:18:32 +0700

    [ADE_001] Adds simple animation

--- Tdowano-kuni/main.cpp
+++ Tdowano-kuni/main.cpp
@@ -1,24 +1,60 @@
+#include <iostream>
+
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+//const unsigned int WINDOW_WIDTH = 1920;
+//const unsigned int WINDOW_HEIGHT = 1080;
+const unsigned int WINDOW_WIDTH = 512;
+const unsigned int WINDOW_HEIGHT = 512;
+const sf::Color BACKGROUND_COLOR = sf::Color(100, 100, 100);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+int main() {
+	// init window
+	sf::VideoMode vm(WINDOW_WIDTH, WINDOW_HEIGHT);
+	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT),
+		"Samurai",
+		//sf::Style::Fullscreen);
+		sf::Style::Close | sf::Style::Resize);
+	sf::View view(sf::FloatRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT));
+	window.setView(view);
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	sf::Texture testTexture;
+	testTexture.loadFromFile("graphics/Samurai/Idle.png");
 
-    return 0;
+	unsigned int currentFrame = 0;
+	unsigned int realFrame = 0;
+
+	sf::Sprite testSprite;
+	testSprite.setTexture(testTexture);
+
+	// game loop
+	while (window.isOpen())
+	{
+		testSprite.setTextureRect(sf::IntRect(sf::Vector2i(testTexture.getSize().x / 6 * realFrame,
+			testTexture.getSize().y * 0),
+			sf::Vector2i(testTexture.getSize().x / 6,
+				testTexture.getSize().y)));
+
+		// keyboard events
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			testSprite.move(-1.0f, 0.0f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			testSprite.move(1.0f, 0.0f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			realFrame = ++currentFrame % 6;
+			std::cout << "Current Frame = " << realFrame << std::endl;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();
+		}
+		window.clear(BACKGROUND_COLOR);
+		window.draw(testSprite);
+		window.display();
+	}
 }
