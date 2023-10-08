6c07c862c98ddefbc2b7465c58b17be7813c1632
aderayevans <aderayevans@gmail.com>
2023-10-08 11:34:14 +0700

    [TK-5] apply the SDL2-template ver 1.0.1

--- window_build.bat
+++ window_build.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 @REM cmake -G "MinGW Makefiles" -S . -B bin/debug/
-cmake --build bin/debug/ && start /b bin/debug/SDL2_Template.exe
+cmake --build bin/debug/ && start /b bin/debug/Tdowanokuni.exe
